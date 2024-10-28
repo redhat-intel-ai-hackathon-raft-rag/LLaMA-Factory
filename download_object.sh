19a2897ff3f4485f6fbd73d8be4ea905c458cb6f
EichiUehara <eichi.uehara@aflo.one>
2024-10-28 18:36:18 +0000

    update train prepare scripts

--- download_object.sh
+++ download_object.sh
@@ -2,4 +2,4 @@ PRESIGNED_URL_FOR_BUCKET=https://objectstorage.ap-osaka-1.oraclecloud.com/p/oBZ7
 curl -X GET -o combined_book.tar.gz $PRESIGNED_URL_FOR_BUCKET
 
 PRESIGNED_URL_FOR_BUCKET=https://objectstorage.ap-osaka-1.oraclecloud.com/p/oBZ7aGYm4ucCE2NhErN8Fh0PeoveMcl2hgUbrXl1IQp8_A4cKVCOArPIL2_OY6gI/n/axe0pnb37jb1/b/bucket-20241015-0858/o/combined_web.tar.gz
-curl -X GET -o combined_book.tar.gz $PRESIGNED_URL_FOR_BUCKET 
+curl -X GET -o combined_web.tar.gz $PRESIGNED_URL_FOR_BUCKET 
